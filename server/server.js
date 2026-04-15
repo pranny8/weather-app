dac38eada53774da47adaab5d23ce6a80c9c43fe
prasanna paudel <prasannapaudel@prasannas-MacBook-Air.local>
2026-04-15 17:47:53 +0545

    fix deploy crash

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5050;
+const apiKey = process.env.API_KEY;
 
 //test route 
 app.get("/", (req, res) => {
